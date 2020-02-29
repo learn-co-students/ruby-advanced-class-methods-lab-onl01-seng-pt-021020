b19f86135014fb31ae6aaceb64d38b80d49829b0
Andre Skeene <halodre24@gmail.com>
2020-02-29 19:18:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,31 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.name = name
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(song_title)
+    song = self.new
+    song.name = song_title
+    song
+  end
+
+  def self.create_by_name(song_title)
+    song = self.create
+    song.name = song_title
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name
+
+  end
 end
