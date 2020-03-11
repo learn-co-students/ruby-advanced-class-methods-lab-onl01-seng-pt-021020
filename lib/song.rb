3161dccc5784956eac6971f7eefed422c907bd1b
kiran ram <kram@lions.molloy.edu>
2020-03-11 01:05:03 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,14 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def initialize
+    self.class.all << self
+  end
+
+  def self.create
+    self.new
+  end
+
   def self.all
     @@all
   end
@@ -9,5 +17,61 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.new_by_name(song_name)
+    new_song = self.new 
+    new_song.name = song_name
+    
+    new_song
+  end
+  
+  def self.create_by_name(name_of_song)
+    new_song = self.new 
+    new_song.name = name_of_song
+    
+    new_song
+  end
+  
+  def self.find_by_name(find_by_name)
+    self.all.find{|song| song.name == find_by_name}
+    
+  end
+  
+  def self.find_or_create_by_name(name)
+    found_song = self.find_by_name(name)
+    
+    if found_song == nil 
+      self.create_by_name(name)
+    else
+      found_song
+    end
+  end
+    
+  def self.alphabetical 
+     @@all.sort_by { |song| song.name }
+    
+  end
+  
+  def self.new_from_filename(name)
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song
+  end
+  
+  def self.create_from_filename(name)
+  #class method should not only parse the filename correctly but should also save the song
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
+    song
+  end
 
+    
+    
+  def self.destroy_all
+       @@all.clear
+  end
+  
 end
